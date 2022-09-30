d3bf7bcf8d92e86597458a497bc3e2c14b2a9ad8
gffice <114347535+gffice@users.noreply.github.com>
2022-10-01 00:20:31 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,10 @@
+@echo off
+echo ----------------------
 set str="AAA BBB CCC" 
 :STR_VISTOR
 for /f "tokens=1,*" %%a in (%str%) do ( 
 echo %%a
+set str="%%b" 
 goto STR_VISTOR
 )
+echo ----------------------
