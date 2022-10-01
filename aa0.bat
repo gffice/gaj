d5055ef45e4e27de40ba0daa5f8091cb6e9ab4d0
gffice <114347535+gffice@users.noreply.github.com>
2022-10-01 23:27:58 +0800

    Update aa0.bat

--- aa0.bat
+++ aa0.bat
@@ -1,3 +1,3 @@
-set temab= |dir /b /s /a:-d *.txt
-echo %temab%
-for /f "delims=\n\ tokens=1,*" %%b in (%temab%) do (echo %%b>>%temab%)
+for /r %2 %%a in (*.txt) do type "%%a" | find "%1" /v >>"%%a.tmp"
+for /r %2 %%a in (*.txt) do del /q "%%a" && move /y "%%a.tmp" "%%a" >nul
+rem for /f "delims=\n\ tokens=1,*" %%b in (%temab%) do (echo %%b>>%temab%)
