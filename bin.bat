3caaa9dc92a0eaadaecadf06a3d5b5652ca46df3
gffice <114347535+gffice@users.noreply.github.com>
2022-10-01 22:28:04 +0800

    Update bin.bat

--- bin.bat
+++ bin.bat
@@ -2,6 +2,6 @@ set targe=''
 setlocal enabledelayedexpansion
 for /f   %%i in (sh0.txt)  do (
 curl https://data.gtimg.cn/flashdata/hushen/latest/daily/sh%%i.js >sh%%i.txt
-for /f "delims=\n\ tokens=1,*" %%b in (%%i.txt) do (echo %%b>>%%i.txt)
+for /f "delims=\n\ tokens=1,*" %%b in (sh%%i.txt) do (echo %%b>>sh%%i.txt)
 ) 
 dir
