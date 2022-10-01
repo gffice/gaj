8f380026ee3c479d72d71a225be3660ed0e3cbc3
gffice <114347535+gffice@users.noreply.github.com>
2022-10-01 20:31:27 +0800

    Update bin.bat

--- bin.bat
+++ bin.bat
@@ -2,6 +2,7 @@ set targe=''
 setlocal enabledelayedexpansion
 for /f   %%i in (sh0.txt)  do (
 set target=%%i
-curl https://data.gtimg.cn/flashdata/hushen/latest/daily/sh!target!.js >sh!target!.txt
+set dlo=!target!
+curl https://data.gtimg.cn/flashdata/hushen/latest/daily/sh%dlo%.js >sh%dlo%.txt
 ) 
 dir
