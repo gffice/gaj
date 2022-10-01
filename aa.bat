bab4f15ca74fd9477045a2acb877b9611eff9b18
gffice <114347535+gffice@users.noreply.github.com>
2022-10-01 20:43:07 +0800

    Update aa.bat

--- aa.bat
+++ aa.bat
@@ -1,11 +1,11 @@
-@echo off
-setlocal EnableDelayedExpansion
- 
-set "CurrCD=%~dp0"
-set "strOld=  "
-set "strNew= "
- 
-:: 将当前目录(包括子目录)下的所有 TXT 文本中的 aaaaa 替换为 zzzzz
-for /f %%i in ('dir /b /s /a:-d *.txt') do (
-  powershell -Command "(gc %%i) -replace '%strOld%', '%strNew%' | Out-File -Encoding ANSII %%i"
-)
+setlocal enabledelayedexpansion
+for /f "eol=* tokens=*" %%i in ('dir /b /s /a:-d *.txt') do (
+rem 设置变量a为每行内容
+set a=%%i
+rem 如果该行有123，则将其改为456
+set "a=!a:/n/=!"
+rem 把修改后的全部行存入$
+echo !a!>>$)
+rem 用$的内容替换原来a.txt内容
+move $ a.txt
+
