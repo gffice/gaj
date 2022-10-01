9342cf0232fb4a256994d9bf1effa39f49383818
gffice <114347535+gffice@users.noreply.github.com>
2022-10-01 22:44:04 +0800

    Update aa0.bat

--- aa0.bat
+++ aa0.bat
@@ -1,3 +1,4 @@
 dir /b /s /a:-d *.txt >fg.txt
 set /p fg= <fg.txt
-for /f "delims=\n\ tokens=1,*" %%b in (%fg%.txt) do (echo %%b>>%fg%.txt)
+del fg.txt
+for /f "delims=\n\ tokens=1,*" %%b in (%fg%) do (echo %%b>>%fg%)
