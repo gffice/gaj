311f439d120cb49fd8b20fe933e15a4252b9bd49
gffice <114347535+gffice@users.noreply.github.com>
2022-10-03 14:18:36 +0800

    Update aa0.bat

--- aa0.bat
+++ aa0.bat
@@ -1,9 +1,5 @@
 dir /b /s /a:-d *.txt >$
-for /f %%i in ($)  do (
+for /f "" %%i in ($) do (
 set dit=%%i
-goto 4
-:5
-)
-:4
 for /f "delims=\n\ tokens=1,*" %%b in (%dit%) do (echo %%b>>%dit%)
-goto 5
+)
