e9fb08eabd2031f4d69e8a9d09e38a46d298f87c
gffice <114347535+gffice@users.noreply.github.com>
2022-10-03 15:17:12 +0800

    Update aa0.bat

--- aa0.bat
+++ aa0.bat
@@ -1,1 +1,3 @@
-for /f "delims=\n\ tokens=1,*" %%b in (%1) do (echo %%b>>%1)
+for /f "delims=\ tokens=1,*" %%b in (%1) do (echo %%b>>%1)
+for /f "delims=n tokens=1,*" %%b in (%1) do (echo %%b>>%1)
+for /f "delims=\ tokens=1,*" %%b in (%1) do (echo %%b>>%1)
