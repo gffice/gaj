d899026fcf307bd57da458de4164471e9af15244
gffice <114347535+gffice@users.noreply.github.com>
2022-10-03 14:24:39 +0800

    Update aa0.bat

--- aa0.bat
+++ aa0.bat
@@ -1,6 +1,6 @@
 setlocal enabledelayedexpansion
-dir /b /s /a:-d *.txt >ll.txt
-for /f "" %%i in (ll.txt) do (
+dir /b /s /a:-d *.txt >>$
+for /f "" %%i in ($) do (
 set dit=%%i
 for /f "delims=\n\ tokens=1,*" %%b in (%dit%) do (echo %%b>>%dit%)
 )
