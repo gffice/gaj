863df28b7fda00f06c0bb92deff686e23c1ae290
gffice <114347535+gffice@users.noreply.github.com>
2022-10-03 15:09:21 +0800

    Update ka.bat

--- ka.bat
+++ ka.bat
@@ -1,6 +1,5 @@
 setlocal enabledelayedexpansion
 dir /b /s /a:-d *.txt >>$
 for /f "" %%i in ($) do (
-set dit=%%i
-for /f "delims=\n\ tokens=1,*" %%b in (%dit%) do (echo %%b>>%dit%)
+cmd /c aa0.bat %%i
 )
