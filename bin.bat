7af21befac5c3036e5851d88887ac2ee1f00cbd0
gffice <114347535+gffice@users.noreply.github.com>
2022-10-03 17:10:16 +0800

    Update bin.bat

--- bin.bat
+++ bin.bat
@@ -4,5 +4,7 @@ for /f   %%i in (sh0.txt)  do (
 curl https://data.gtimg.cn/flashdata/hushen/latest/daily/sh%%i.js >sh%%i.txt
 cmd /c aa0.bat %%i
 del sh%%i.txt
+cmd /c de2.bat %%i.txt
+cmd /c look0.bat %%i.txt
 ) 
 dir
