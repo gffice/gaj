a8f49277e16c68e9ae80d2702c301a9719eada09
gffice <114347535+gffice@users.noreply.github.com>
2022-10-03 14:06:43 +0800

    Update aa0.bat

--- aa0.bat
+++ aa0.bat
@@ -2,6 +2,9 @@ dir /b /s /a:-d *.txt >ghl.txt
 rem setlocal enabledelayedexpansion
 for /f   %%i in (ghl.txt)  do [
 set dit= %%i
-for /f "delims=\n\ tokens=1,*" %%b in (%dit%) do (echo %%b>>%dit%)
+goto 4
+:5
 ]
-
+:4
+for /f "delims=\n\ tokens=1,*" %%b in (%dit%) do (echo %%b>>%dit%)
+goto 5
