ae24d53cb87cf837691849b393a128659187e3f1
gffice <114347535+gffice@users.noreply.github.com>
2022-10-03 15:41:25 +0800

    Update bin.bat

--- bin.bat
+++ bin.bat
@@ -3,5 +3,6 @@ setlocal enabledelayedexpansion
 for /f   %%i in (sh0.txt)  do (
 curl https://data.gtimg.cn/flashdata/hushen/latest/daily/sh%%i.js >sh%%i.txt
 cmd /c aa0.bat %%i
+del sh%%i.txt
 ) 
 dir
